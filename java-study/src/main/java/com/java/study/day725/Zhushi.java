2e88bdead27f78860d8995149f8cecd815149484
xin <1595258579@qq.com>
2022-07-25 20:40:54 +0800

    提交注释说明

---
+++ java-study/src/main/java/com/java/study/day725/Zhushi.java
@@ -0,0 +1,16 @@
+package com.java.study.day725;
+
+public class Zhushi {
+    public static void main(String[] args) {
+        System.out.println("注释");
+//        单行注释
+
+        /*
+        多行注释
+        */
+
+        /**
+         文本注释
+         */
+    }
+}
