95703ff9a898a9bbf6926c428fb6b584467310c6
xin <1595258579@qq.com>
2022-08-03 21:11:55 +0800

    fix

---
+++ java-study/src/main/java/com/java/study/day725/test1.java
@@ -0,0 +1,15 @@
+package com.java.study.day725;
+
+public class test1 {
+
+    public class Study {
+        //    类的命名大驼峰单词首字母大写
+//    常量名  全大写
+        public static final String MSG="dadadadada";
+    }
+    class UserInfo{
+        //    变量名小驼峰 第二个单词大写
+        private String userName;
+    }
+
+}
