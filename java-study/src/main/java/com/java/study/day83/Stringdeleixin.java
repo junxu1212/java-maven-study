791e481c31dca170cc5e2b606ada2fa6f33602f1
xin <1595258579@qq.com>
2022-08-03 21:08:44 +0800

    swich语句

--- java-study/src/main/java/com/java/study/day83/Stringdeleixin.java
+++ java-study/src/main/java/com/java/study/day83/Stringdeleixin.java
@@ -17,6 +17,24 @@ public class Stringdeleixin {
                 str+="顾客";break;
         }
         System.out.println(str);
+        System.out.println("请输入一个数：");
+        int a= sc.nextInt();
+        switch (a){
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("工作日");
+                break;
+            case 6:
+            case 7:
+                System.out.println("休息日");
+                break;
+//                switch穿透现象  流程是走了一个case遇到breack才会停止但是这里面是没有停止的
+//            会一直运行到5才break  值不同但是结果是一样的就可以用case穿透
+        }
     }
 //    java7以前是没有String类型的 只支持int 和枚举类型   输入的byte char类型自动转为int类型
+
 }
