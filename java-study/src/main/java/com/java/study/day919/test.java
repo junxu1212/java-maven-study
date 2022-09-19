771a05f36d189edcbbf901d2375dd013c3d73abc
xin <1595258579@qq.com>
2022-09-19 21:03:11 +0800

    fix

--- java-study/src/main/java/com/java/study/day919/test.java
+++ java-study/src/main/java/com/java/study/day919/test.java
@@ -1,4 +1,14 @@
 package com.java.study.day919;
 
+import java.util.Date;
+
 public class test {
+    public static void main(String[] args) {
+        Date date=new Date();
+        java.sql.Date date1=new java.sql.Date(System.currentTimeMillis());
+        System.out.println(date1);
+//        在一个类中使用两个重名的类
+//        第一个类keyiimport导入
+//        第二个类必须使用包名加类名导入（全限定名）
+    }
 }
